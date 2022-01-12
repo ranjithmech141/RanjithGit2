f381cdcabbe759a8fe0ec08754714d0345c9cb1f
ranjithmech141 <ranjithmech141@gmail.com>
2022-01-12 11:00:40 +0530

    done

--- src/org/example/Company.java
+++ src/org/example/Company.java
@@ -1,18 +1,8 @@
 package org.example;
 
 public class Company {
-<<<<<<< HEAD
 	private void m3() {
 		System.out.println("C is done");
-=======
-
-	
-	private void m1() {
-	System.out.println("m1 Created by A branch");
-	}
-	private void m2() {
-		System.out.println("Method created by B branch");
->>>>>>> 57d472c4295151923842c70c91e3cf11ae126909
 	}
 	
 	public static void main(String[] args) {
